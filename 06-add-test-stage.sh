ff3a4d729a66f46c85213595c8799261c6bf13f8
TR Platt <trplatt@amazon.com>
2022-03-20 11:30:53 +0000

    more robustness

--- 06-add-test-stage.sh
+++ 06-add-test-stage.sh
@@ -8,8 +8,11 @@
 PREFIX=a-new-startup
  
 echo "Creating complex CodePipeline pipeline (Source/Build App/Build Test Container/Deploy/Test) ..."
-aws cloudformation deploy --template-file pipeline-with-tests.yaml --stack-name $PREFIX-pipeline --capabilities CAPABILITY_NAMED_IAM
-
-
-
- 
+STACK_NAME=$PREFIX-pipeline
+aws cloudformation deploy --template-file pipeline-with-tests.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation wait stack-exists --stack-name $STACK_NAME
+STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[].StackStatus" --output text)
+if [[ $STACK_STATUS != "CREATE_COMPLETE" ]] && [[ $STACK_STATUS != "UPDATE_COMPLETE" ]]; then
+        echo "Create or Update of Stack $STACK_NAME failed: $STACK_STATUS.  Cannot continue..."
+        exit 1
+fi
--- 07-full-pipeline.sh
+++ 07-full-pipeline.sh
@@ -8,4 +8,11 @@
 PREFIX=a-new-startup
 
 echo "Creating full CodePipeline pipeline ..."
+STACK_NAME=$PREFIX-pipeline
 aws cloudformation deploy --template-file pipeline-full.yaml --stack-name $PREFIX-pipeline --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation wait stack-exists --stack-name $STACK_NAME
+STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[].StackStatus" --output text)
+if [[ $STACK_STATUS != "CREATE_COMPLETE" ]] && [[ $STACK_STATUS != "UPDATE_COMPLETE" ]]; then
+        echo "Create or Update of Stack $STACK_NAME failed: $STACK_STATUS.  Cannot continue..."
+        exit 1
+fi

1861267bdf09322cb001b4743978b40af671afba
TR Platt <trplatt@amazon.com>
2022-03-26 01:06:03 +0000

    Made repo setup more robust by falling back to zip file of source, if needed

--- 01-repo.sh
+++ 01-repo.sh
@@ -16,10 +16,20 @@ PREFIX=a-new-startup
 # Make sure we don't have this folder local
 rm -rf a-new-startup-github 
 
+echo "Attemping to retrieve latest a-new-startup app source code from git@github.com:tplatt37/a-new-startup.git using ssh..."
 git clone git@github.com:tplatt37/a-new-startup.git a-new-startup-github
-
-# NOTE: When we zip, we ignore .git folder, but include other hidden files and folders! 
-cd a-new-startup-github && zip -r --exclude=*.git/* ../a-new-startup.zip ./* .[^.]* && cd ..
+if [ $? -eq 128 ]; then
+        echo "But... that failed, so we'll use a possibly out of date zip instead."
+        cp a-new-startup-fallback.zip a-new-startup.zip
+else
+        # If it was successful, zip up what was cloned
+        # NOTE: When we zip, we ignore .git folder, but include other hidden files and folders! 
+        echo "Success! Let's zip it up!"
+        cd a-new-startup-github && zip -r --exclude=*.git/* ../a-new-startup.zip ./* .[^.]* && cd ..
+        # Save this for next time, in case we can't get the code live.
+        # (This makes the maintainer's life easier)
+        cp a-new-startup.zip a-new-startup-fallback.zip 
+fi
 
 echo "Copying application source zip to S3 bucket"
 aws s3 cp a-new-startup.zip s3://$BUCKET
@@ -29,10 +39,17 @@ aws s3 cp a-new-startup.zip s3://$BUCKET
 # Make sure we don't have this folder local
 rm -rf a-new-startup-ui-tests-github 
 
+echo "Attemping to retrieve ui testing code from git@github.com:tplatt37/a-new-startup-ui-tests.git using ssh..."
 git clone git@github.com:tplatt37/a-new-startup-ui-tests.git a-new-startup-ui-tests-github
-
-# NOTE: When we zip, we ignore .git folder, but include other hidden files and folders! 
-cd a-new-startup-ui-tests-github && zip -r --exclude=*.git/* ../a-new-startup-ui-tests.zip ./* .[^.]* && cd ..
+if [ $? -eq 128 ]; then
+        echo "But... that failed, so we'll use a possibly out of date zip instead."
+        cp a-new-startup-ui-tests-fallback.zip a-new-startup-ui-tests.zip
+else
+        # NOTE: When we zip, we ignore .git folder, but include other hidden files and folders! 
+        echo "Success! ZIPPING UP!"
+        cd a-new-startup-ui-tests-github && zip -r --exclude=*.git/* ../a-new-startup-ui-tests.zip ./* .[^.]* && cd ..
+        cp a-new-startup-ui-tests.zip a-new-startup-ui-tests-fallback.zip
+fi
 
 # This is a zip of the testing code (python/selenim) used to test the UI.
 aws s3 cp a-new-startup-ui-tests.zip s3://$BUCKET

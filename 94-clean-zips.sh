cfe2e1e7ab2bd439c0dba7068acf84fc8e036343
TR Platt <trplatt@amazon.com>
2023-10-09 10:41:04 +0000

    feature: Added zip cleanup routine for maintainer use

---
+++ 94-clean-zips.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Clean up temp files. These get rebuilt automatically.
+
+echo "Cleaning up zips..."
+
+rm -rf *.zip
+rm -rf a-new-startup-github
+rm -rf a-new-startup-ui-tests-github

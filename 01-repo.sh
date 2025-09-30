d1405731499357e7cc012114aa950368f766b53f
TR Platt <trplatt@amazon.com>
2025-09-30 00:40:53 +0000

    retrieve from public

--- 01-repo.sh
+++ 01-repo.sh
@@ -16,8 +16,8 @@ PREFIX=a-new-startup
 # Make sure we don't have this folder local
 rm -rf a-new-startup-github 
 
-echo "Attemping to retrieve latest a-new-startup app source code from git@github.com:tplatt37/a-new-startup.git using ssh..."
-git clone git@github.com:tplatt37/a-new-startup.git a-new-startup-github
+echo "Attemping to retrieve latest a-new-startup app source code"
+git clone https://github.com/tplatt37/a-new-startup.git a-new-startup-github
 if [ $? -eq 128 ]; then
         echo "But... that failed, so we'll use a possibly out of date zip instead."
         cp a-new-startup-fallback.zip a-new-startup.zip
@@ -39,8 +39,8 @@ aws s3 cp a-new-startup.zip s3://$BUCKET
 # Make sure we don't have this folder local
 rm -rf a-new-startup-ui-tests-github 
 
-echo "Attemping to retrieve ui testing code from git@github.com:tplatt37/a-new-startup-ui-tests.git using ssh..."
-git clone git@github.com:tplatt37/a-new-startup-ui-tests.git a-new-startup-ui-tests-github
+echo "Attemping to retrieve ui testing code"
+git clone https://github.com/tplatt37/a-new-startup-ui-tests.git a-new-startup-ui-tests-github
 if [ $? -eq 128 ]; then
         echo "But... that failed, so we'll use a possibly out of date zip instead."
         cp a-new-startup-ui-tests-fallback.zip a-new-startup-ui-tests.zip
